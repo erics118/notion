b7197026dec7b7b0eca18d2acd7e169441609547
erics118 <52634785+erics118@users.noreply.github.com>
2023-06-17 07:29:48 -0400

    add utils to get workspace, page, block from url, add workspace string id

--- notion-model/src/ids.rs
+++ notion-model/src/ids.rs
@@ -8,19 +8,22 @@ use uuid::Uuid;
 macro_rules! uuid_id {
     ($($name:ident;)*) => {
         $(
-
             #[derive(
                 Copy, Clone, Default, Debug, Hash, Eq, PartialEq,
                 Ord, PartialOrd, Deserialize, Serialize,
             )]
-            pub struct $name(pub Uuid);
+            pub struct $name(Uuid);
 
             impl $name {
                 /// Immutably borrow inner Id.
                 #[inline]
                 #[must_use]
-                pub const fn as_uuid(&self) -> &Uuid {
+                pub const fn into_inner(&self) -> &Uuid {
                     &self.0
+                }
+
+                pub fn from_str_unchecked(uuid_str: &str) -> Self {
+                    Self(Uuid::parse_str(uuid_str).unwrap())
                 }
             }
 
@@ -72,6 +75,79 @@ macro_rules! uuid_id {
                     fmt::Display::fmt(&self.0, f)
                 }
             }
+
+        )*
+    }
+}
+
+macro_rules! string_id {
+    ($($name:ident;)*) => {
+        $(
+            #[derive(Clone, Default, Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Deserialize, Serialize)]
+            pub struct $name(String);
+
+            impl $name {
+                /// Immutably borrow inner Id.
+                #[inline]
+                #[must_use]
+                pub const fn into_inner(&self) -> &String {
+                    &self.0
+                }
+
+                pub fn from_str_unchecked(str: &str) -> Self {
+                    Self(str.to_string())
+                }
+            }
+
+            impl std::str::FromStr for $name {
+                type Err = uuid::Error;
+
+                fn from_str(str: &str) -> Result<Self, Self::Err> {
+                    Ok(Self(str.to_string()))
+                }
+            }
+
+            impl TryFrom<&'_ str> for $name {
+                type Error = uuid::Error;
+
+                fn try_from(str: &'_ str) -> Result<Self, Self::Error> {
+                    Ok(Self(str.to_string()))
+                }
+            }
+
+            impl<T: ?Sized> AsMut<T> for $name
+            where
+                String: AsMut<T>,
+            {
+                fn as_mut(&mut self) -> &mut T {
+                    self.0.as_mut()
+                }
+            }
+
+            impl AsRef<Self> for $name {
+                fn as_ref(&self) -> &Self {
+                    self
+                }
+            }
+
+            impl<'a> From<&'a Self> for $name {
+                fn from(id: &'a Self) -> Self {
+                    id.clone()
+                }
+            }
+
+            impl From<String> for $name {
+                fn from(string: String) -> Self {
+                    Self(string)
+                }
+            }
+
+            impl fmt::Display for $name {
+                fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                    fmt::Display::fmt(&self.0, f)
+                }
+            }
+
         )*
     }
 }
@@ -81,4 +157,8 @@ uuid_id! {
     DatabaseId;
     PageId;
     UserId;
+}
+
+string_id! {
+    WorkspaceId;
 }
--- notion/src/utils.rs
+++ notion/src/utils.rs
@@ -1,3 +1,252 @@
-// TODO: get workspace from URL, both types of URLs, shared url and edit url
-// TODO: get block id from URL,
-// TODO: get page id from URL
+use std::str::FromStr;
+
+use notion_model::ids::{BlockId, PageId, WorkspaceId};
+
+pub fn get_block_id_from_url(url: &str) -> Option<BlockId> {
+    println!("{:#?}", url);
+    let split_url: Vec<&str> = url.split('#').collect();
+    println!("{:#?}", split_url);
+    if split_url.len() == 2 {
+        return split_url.get(1).and_then(|s| BlockId::from_str(s).ok());
+    }
+    None
+}
+
+pub fn get_page_id_from_url(url: &str) -> Option<PageId> {
+    let split_url: Vec<&str> = url.split(&['/', '?']).collect();
+
+    for i in 0..split_url.len() {
+        if vec![
+            "notion.so",
+            "notion.site",
+            "www.notion.so",
+            "www.notion.site",
+        ]
+        .contains(&split_url[i])
+        {
+            if i + 1 < split_url.len() {
+                return split_url.get(i + 2).and_then(|s| PageId::from_str(s).ok());
+            }
+        }
+    }
+
+    None
+}
+
+pub fn get_workspace_from_url(url: &str) -> Option<WorkspaceId> {
+    let split_url: Vec<&str> = url.split('/').collect();
+
+    for i in 0..split_url.len() {
+        if vec![
+            "notion.so",
+            "notion.site",
+            "www.notion.so",
+            "www.notion.site",
+        ]
+        .contains(&split_url[i])
+        {
+            if i + 2 < split_url.len() {
+                return split_url
+                    .get(i + 1)
+                    .and_then(|s| WorkspaceId::from_str(s).ok());
+            }
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    pub fn a() {
+        let url = "https://www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4";
+        let block_id = get_block_id_from_url(url).unwrap_or_default();
+        println!("Block ID: {}", block_id);
+    }
+
+    #[test]
+    pub fn block_full() {
+        assert_eq!(
+            get_block_id_from_url(
+                "https://www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(BlockId::from_str_unchecked(
+                "6e9612c81c7d4356ba9153eab009e6f4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn block_no_https() {
+        assert_eq!(
+            get_block_id_from_url(
+                "www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(BlockId::from_str_unchecked(
+                "6e9612c81c7d4356ba9153eab009e6f4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn block_no_www() {
+        assert_eq!(
+            get_block_id_from_url(
+                "https://notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(BlockId::from_str_unchecked(
+                "6e9612c81c7d4356ba9153eab009e6f4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn block_no_https_no_www() {
+        assert_eq!(
+            get_block_id_from_url(
+                "notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(BlockId::from_str_unchecked(
+                "6e9612c81c7d4356ba9153eab009e6f4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn block_no_params() {
+        assert_eq!(
+            get_block_id_from_url(
+                "https://www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(BlockId::from_str_unchecked(
+                "6e9612c81c7d4356ba9153eab009e6f4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn block_none() {
+        assert_eq!(
+            get_block_id_from_url(
+                "https://www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4"
+            ),
+            None
+        );
+    }
+
+    #[test]
+    pub fn page_full() {
+        assert_eq!(
+            get_page_id_from_url(
+                "https://www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(PageId::from_str_unchecked(
+                "67ace61a7fd24ab78e892b1dc9b252e4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn page_no_https() {
+        assert_eq!(
+            get_page_id_from_url(
+                "www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(PageId::from_str_unchecked(
+                "67ace61a7fd24ab78e892b1dc9b252e4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn page_no_www() {
+        assert_eq!(
+            get_page_id_from_url(
+                "https://notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(PageId::from_str_unchecked(
+                "67ace61a7fd24ab78e892b1dc9b252e4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn page_no_https_no_www() {
+        assert_eq!(
+            get_page_id_from_url(
+                "notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(PageId::from_str_unchecked(
+                "67ace61a7fd24ab78e892b1dc9b252e4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn page_no_block() {
+        assert_eq!(
+            get_page_id_from_url("https://www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4"),
+            Some(PageId::from_str_unchecked(
+                "67ace61a7fd24ab78e892b1dc9b252e4"
+            ))
+        );
+    }
+
+    #[test]
+    pub fn page_none() {
+        assert_eq!(get_page_id_from_url("https://www.notion.so/erics118"), None);
+    }
+
+    #[test]
+    pub fn workspace_full() {
+        assert_eq!(
+            get_workspace_from_url(
+                "https://www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(WorkspaceId::from_str_unchecked("erics118"))
+        );
+    }
+
+    #[test]
+    pub fn workspace_no_https() {
+        assert_eq!(
+            get_workspace_from_url(
+                "www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(WorkspaceId::from_str_unchecked("erics118"))
+        );
+    }
+
+    #[test]
+    pub fn workspace_no_www() {
+        assert_eq!(
+            get_workspace_from_url(
+                "https://notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(WorkspaceId::from_str_unchecked("erics118"))
+        );
+    }
+
+    #[test]
+    pub fn workspace_no_https_no_www() {
+        assert_eq!(
+            get_workspace_from_url(
+                "notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(WorkspaceId::from_str_unchecked("erics118"))
+        );
+    }
+
+    #[test]
+    pub fn workspace_https_no_www() {
+        assert_eq!(
+            get_workspace_from_url(
+                "www.notion.so/erics118/67ace61a7fd24ab78e892b1dc9b252e4?pvs=4#6e9612c81c7d4356ba9153eab009e6f4"
+            ),
+            Some(WorkspaceId::from_str_unchecked("erics118"))
+        );
+    }
+}
